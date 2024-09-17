a7d2b9419cf8c4dd02ce90cbcca9d0ed2b20a3a7
02Blacksnakes03 <82246205+02Blacksnakes03@users.noreply.github.com>
2024-09-17 19:38:49 +0200

    Improve setting up a Localhost

---
+++ resources/[jobs]/[Zivi_Jobs]/esx_tattooshop_new/locales/en.lua
@@ -0,0 +1,11 @@
+Locales['en'] = {
+  ['tattoo_shop_prompt'] = 'Drücke ~INPUT_PICKUP~ um auf den ~y~Tattoo Shop~s~ zuzugrifen.',
+  ['money_amount']       = '<span style="color:green;">$%s</span>',
+  ['part']               = 'Part %s',
+  ['go_back_to_menu']    = '< Gehe zurück',
+  ['tattoo_item']        = 'Tattoo %s - %s',
+  ['tattoos']            = 'Tattoos',
+  ['tattoo_shop']        = 'Tattoo Shop',
+  ['bought_tattoo']      = 'Du hast ein ~y~Tattoo~s~ gekauft für ~r~$%s~s~',
+  ['not_enough_money']   = 'Du hast ~r~nicht genug Geld~s~ f+r das Tattoo! Dir fehlen ~r~$%s~s~'
+}
