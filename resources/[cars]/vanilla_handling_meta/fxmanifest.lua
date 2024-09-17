bb916d20dcc1cac8cfb01467410f553664b2097d
02Blacksnakes03 <82246205+02Blacksnakes03@users.noreply.github.com>
2024-09-17 13:12:19 +0200

    Change vanilla Metas

---
+++ resources/[cars]/vanilla_handling_meta/fxmanifest.lua
@@ -0,0 +1,32 @@
+fx_version "cerulean"
+game "gta5"
+author "chirimu"
+description "vehicle handling meta patch"
+version "1"
+
+
+files {
+	'vanilla.handling.meta',
+	'mpapartment.handling.meta',
+	'mpassault.handling.meta',
+	'mpbattle.handling.meta',
+	'mpexecutive.handling.meta',
+	'mpgunrunning.handling.meta',
+	'mphalloween.handling.meta',
+	'mpheist.handling.meta',
+	'mpheist3.handling.meta',
+	'mpheist4.handling.meta'
+}
+
+
+data_file 'HANDLING_FILE' 'vanilla.handling.meta'
+data_file 'HANDLING_FILE' 'mpheist4.handling.meta'
+data_file 'HANDLING_FILE' 'mpapartment.handling.meta'
+data_file 'HANDLING_FILE' 'mpassault.handling.meta'
+data_file 'HANDLING_FILE' 'mpbattle.handling.meta'
+data_file 'HANDLING_FILE' 'mpexecutive.handling.meta'
+data_file 'HANDLING_FILE' 'mpgunrunning.handling.meta'
+data_file 'HANDLING_FILE' 'mphalloween.handling.meta'
+data_file 'HANDLING_FILE' 'mpheist.handling.meta'
+data_file 'HANDLING_FILE' 'mpheist3.handling.meta'
+data_file 'HANDLING_FILE' 'mpheist4.handling.meta'
