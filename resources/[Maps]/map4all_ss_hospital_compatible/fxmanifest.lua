329078cc14f105ac52e0fe4d2bd50c05a34fef52
02Blacksnakes03 <82246205+02Blacksnakes03@users.noreply.github.com>
2024-09-18 12:40:48 +0200

    changes

---
+++ resources/[Maps]/map4all_ss_hospital_compatible/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'bodacious'
+game 'gta5'
+this_is_a_map 'yes'
+
+author 'Map4All'
+description 'The best supplier'
+version '69.69.69'
+
+
+
+escrow_ignore {
+    'stream/*.ybn',
+    'stream/*.ydr',
+    'stream/*.ydd',
+    'stream/*.ytd'
+}
